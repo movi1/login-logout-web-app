bc3df72459eabe1455a496555c3eabd0b099f060
movi1 <98534830+movi1@users.noreply.github.com>
2023-12-04 15:44:51 +0000

    add animation and update readme

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,62 +1,68 @@
 
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import 'animate.css';
 import './login.css';
 
 const Login = () => {
+    // State variables for managing username, password, and error message
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    // const [isLoggedIn, setIsLoggedIn] = useState(false); // Use useState to manage the state
     const [error, setError] = useState('');
 
+    // React Router hook for navigation
     const navigate = useNavigate();
 
+    // Function to handle the login process
     const handleLogin = () => {
-        // Your logic to check username and password
+        // logic to check username and password
         if (username === 'movi' && password === '1234') {
-            // Successful login
+            // Successful login, navigate to the logout page
             navigate('/logout', { state: { username } });
         } else {
-            // Unsuccessful login
+            // Unsuccessful login, set error message and navigate to loginUnsuccessful page
             setError('Login failed, please try again');
-            navigate('/loginUnsuccessful', { state: { error:"Check your details and give it another go."}})
+            navigate('/loginUnsuccessful', { state: { error: "Check your details and give it another go." } })
             return;
         }
 
-        // Clear input fields
+        // Clear input fields after login attempt
         setUsername('');
         setPassword('');
     };
 
-
+    // Function to handle input changes and clear error messages
     const handleInputChange = () => {
-        // Clear error message when there is a change in the input fields
         setError('');
     };
 
     return (
-        <div className="container">
-            <div className='login'>
-            <h1> 🌈Log in to Paint Your Imagination!</h1>
-
-                <input
-                    type="text"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(e) => { setUsername(e.target.value); handleInputChange(); }} />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => { setPassword(e.target.value); handleInputChange(); }} />
-                <button type="submit" onClick={handleLogin}>Sign in</button>
-
-                {error && <p className="error">{error}</p>}
+        <div className='main-container'>
+            {/* Login container with animation */}
+            <div className="container animate__animated animate__fadeInDown">
+                <div className='login'>
+                    <h1>✨Explore, Create, and Log in!</h1>
+                    {/* Username input field */}
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => { setUsername(e.target.value); handleInputChange(); }} />
+                    {/* Password input field */}
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => { setPassword(e.target.value); handleInputChange(); }} />
+                    {/* Login button */}
+                    <button type="submit" onClick={handleLogin}>Sign in</button>
+                    {/* Display error message if login fails */}
+                    {error && <p className="error">{error}</p>}
+                </div>
             </div>
-
         </div>
     );
 };
+
 
 export default Login;
--- src/components/loginUnsuccessful.jsx
+++ src/components/loginUnsuccessful.jsx
@@ -2,38 +2,43 @@
 import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import './login.css';
+import 'animate.css';
 
 const LoginFailed = () => {
+    // React Router hooks for navigation and location
     const navigate = useNavigate();
     const location = useLocation();
     const { state } = location;
-
-    // Destructure error from state
+    
+    // Extract 'error' from the state, or set it to an empty object if undefined
     const { error } = state || {};
 
-   
 
-
+    // Function to handle navigation back to the login page
     const handleBackToLogin = () => {
         // Navigate back to login page
         navigate('/');
     }
 
     return (
-        <div className="container">
-            <div className="failed">
-                <h1>Oops! Artistic License Denied 🚫 </h1>
-                <div class="error-message">
-                    {error}
+        <div className='main-container'>
+            {/* Container for the failed login message */}
+            <div className="container animate__animated animate__fadeInUp">
+                <div className="failed">
+                    {/* Display a message for unsuccessful login */}
+                    <h1>Oops! Artistic License Denied 🚫 </h1>
+                    {/* Display the error message */}
+                    <div className="error-message">
+                        {error}
+                    </div>
+                    {/* Button to navigate back to the login page */}
+                    <button onClick={handleBackToLogin}>Back to Login</button>
                 </div>
-
-                <button onClick={handleBackToLogin}>
-                    Back to Login
-                </button>
             </div>
         </div>
     );
 };
+
 
 export default LoginFailed;
 
