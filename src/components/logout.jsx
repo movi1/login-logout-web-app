d0bbb47a7154e5e6eb1c08ea20bc11ef937558fc
movi1 <98534830+movi1@users.noreply.github.com>
2023-12-11 11:50:48 +0000

    add user authentication with Redux

--- src/components/logout.jsx
+++ src/components/logout.jsx
@@ -1,27 +1,35 @@
 import React from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { LOGOUT } from '../store';
 import 'animate.css';
 import './login.css';
 
 const Logout = () => {
-  // React Router hooks for navigation and location
-  const navigate = useNavigate();
-  const location = useLocation();
-  const { state } = location;
+    // React Router hooks for navigation
+    const navigate = useNavigate();
 
-  // Function to handle the logout process
-  const handleLogout = () => {
-  
-    navigate('/'); // Navigate to the login page after logout
-  };
+    // Access the loggedInUser state from the Redux store
+    const loggedInUser = useSelector((state) => state.loggedInUser);
+    
+    // Use useDispatch to dispatch actions to the Redux store
+    const dispatch = useDispatch();
 
+    // Function to handle the logout process
+    const handleLogout = () => {
+        // Dispatch the LOGOUT action to update the Redux store
+        dispatch({ type: LOGOUT });
+
+        // Navigate to the login page after logout
+        navigate('/');
+    };
   return (
     <div className="main-container">
       {/* Logout container */}
       <div className="container">
         <div className="logout">
-          {/* Display a welcome message with the username */}
-          <h1>Welcome 🎨 {state && state.username}!</h1>
+        {/* // This checks if loggedInUser is truthy before returning the welcome message */}
+                <h1>Welcome {loggedInUser && `🎨 ${loggedInUser}`}!</h1>
           <h2>Click to the button below to sign out</h2>
           {/* Button to navigate back to the login page */}
           <button onClick={handleLogout}>Sign Out</button>
